3322091ab42909bb16214a8d844a687c5b7a2fbd
devwithu <devwithu@naver.com>
2020-02-13 17:24:25 +0900

    first commit

---
+++ Assets/Scripts/Ball.cs
@@ -0,0 +1,43 @@
+﻿using Photon.Pun;
+using UnityEngine;
+
+public class Ball : MonoBehaviourPun
+{
+    public bool IsMasterClientLocal => PhotonNetwork.IsMasterClient && photonView.IsMine;
+
+    private Vector2 direction = Vector2.right;
+    private readonly float speed = 10f;
+    private readonly float randomRefectionIntensity = 0.1f;
+    
+    private void FixedUpdate()
+    {
+        if (!IsMasterClientLocal || PhotonNetwork.PlayerList.Length < 2)
+        {
+            return;
+        }
+
+        var distance = speed * Time.deltaTime;
+        var hit = Physics2D.Raycast(transform.position, direction, distance);
+
+        if (hit.collider != null)
+        {
+            var goalPost = hit.collider.GetComponent<Goalpost>();
+            if (goalPost != null)
+            {
+                if (goalPost.playerNumber == 1)
+                {
+                    GameManager.Instance.AddScore(2,1);
+                    
+                } else if (goalPost.playerNumber == 2)
+                {
+                    GameManager.Instance.AddScore(1,1);
+                }
+            }
+            direction = Vector2.Reflect(direction, hit.normal);
+            direction += Random.insideUnitCircle * randomRefectionIntensity;
+            
+        }
+
+        transform.position = (Vector2)transform.position + direction * distance;
+    }
+}
---
+++ Assets/Scripts/LobbyManager.cs
@@ -0,0 +1,66 @@
+﻿using Photon.Pun;
+using Photon.Realtime;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class LobbyManager : MonoBehaviourPunCallbacks
+{
+    private readonly string gameVersion = "1";
+
+    public Text connectionInfoText;
+    public Button joinButton;
+    
+    private void Start()
+    {
+        PhotonNetwork.GameVersion = gameVersion;
+        PhotonNetwork.ConnectUsingSettings();
+        joinButton.interactable = false;
+        connectionInfoText.text = "Connecting To Master Server...";
+        
+    }
+    
+    public override void OnConnectedToMaster()
+    {
+        joinButton.interactable = true;
+        connectionInfoText.text = "Online : Connected to master server";
+        
+    }
+    
+    public override void OnDisconnected(DisconnectCause cause)
+    {
+        joinButton.interactable = false;
+        connectionInfoText.text = $"Offline : Connectioon Disabled {cause.ToString()} - Try reconnecting";
+        PhotonNetwork.ConnectUsingSettings();
+    }
+    
+    public void Connect()
+    {
+        joinButton.interactable = false;
+        if (PhotonNetwork.IsConnected)
+        {
+            connectionInfoText.text = "Connecting to Random Room...";
+            PhotonNetwork.JoinRandomRoom();
+        }
+        else
+        {
+            connectionInfoText.text = $"Offline : Connectioon Disabled - Try reconnecting";
+            PhotonNetwork.ConnectUsingSettings();
+
+        }
+    }
+    
+    public override void OnJoinRandomFailed(short returnCode, string message)
+    {
+        connectionInfoText.text = "There is no empty room. Creating new Room.";
+        PhotonNetwork.CreateRoom(null, new RoomOptions {MaxPlayers = 2});
+        
+            
+    }
+    
+    public override void OnJoinedRoom()
+    {
+        connectionInfoText.text = "Connected with Room";
+        PhotonNetwork.LoadLevel("Main");
+        
+    }
+}
